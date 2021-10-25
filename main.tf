4d26f21875d20289927e54dbbee8801102eda755
Michael Wells <me@mikeywells.dev>
2021-10-24 18:06:48 -0700

    add new config

--- main.tf
+++ main.tf
@@ -1,5 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.0"
+    }
+  }
+}
+provider "aws" {
+  region = "us-west-2"
+}
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"]
+}
 resource "aws_instance" "helloworld" {
-  ami           = "ami-09dd2e08d601bff67"
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   tags = {
     Name = "HelloWorld"
